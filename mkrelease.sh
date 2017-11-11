c6cfa1a9034b019a102a95ef576ea4b7d580d5bb
Andrew Chambers <andrewchambers@fastmail.com>
2017-11-11 17:57:45 +1300

    script for building a release

---
+++ mkrelease.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+set -e
+set -u
+
+version=`git describe --always --dirty`
+
+for os in linux darwin freebsd openbsd windows 
+do
+	for arch in amd64
+	do
+		build=wsemail-$version-$os-$arch
+		builddir=./release/$build
+		mkdir -p $builddir
+		export GOOS=$os
+		export GOARCH=$arch
+		export CGO_ENABLED=0
+		go build -a -o $builddir/wsemail
+		cd release
+		tar czf $build.tar.gz $build
+		zip -r $build.zip $build
+		cd ..
+		rm -rf $builddir
+	done
+done
+
+cd release
+sha256sum *.tar.gz *.zip > SHA256
